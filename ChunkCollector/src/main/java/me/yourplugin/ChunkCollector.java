ef7752450fb9ff6ea75eed19a2c1f44a3afb1665
hydrogenmc <genhydro95@gmail.com>
2025-03-16 03:30:40 +0000

    Initial ChunkCollector plugin setup

---
+++ ChunkCollector/src/main/java/me/yourplugin/ChunkCollector.java
@@ -0,0 +1,18 @@
+package me.yourplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChunkCollector extends JavaPlugin {
+        @Override
+            public void onEnable() {
+                        getLogger().info("ChunkCollector has been enabled!");
+            }
+
+                @Override
+                    public void onDisable() {
+                                getLogger().info("ChunkCollector has been disabled!");
+                    }
+                }
+                    }
+            }
+}
